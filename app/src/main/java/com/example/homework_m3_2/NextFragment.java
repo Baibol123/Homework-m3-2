ed0f339fa342d57318612905577e7368351562ed
baibol123 <baibolchik.002@gmail.com>
2023-12-13 22:31:15 +0600

    Initial commit

---
+++ app/src/main/java/com/example/homework_m3_2/NextFragment.java
@@ -0,0 +1,44 @@
+package com.example.homework_m3_2;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+
+public class NextFragment extends Fragment {
+
+    private TextView textResult;
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_next, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        initView(view);
+        initListener();
+
+
+    }
+
+
+    private void initListener(){
+        Bundle bundle = getArguments();
+        if (bundle != null) {
+            int number = bundle.getInt("number", 0);
+            textResult.setText(String.valueOf(number));
+        }
+    }
+    private void initView(View view) {
+        textResult = view.findViewById(R.id.textResult);
+    }
+}
